a3e2f6ead554010f40a3c84467e24cfe0e955463
Lilianascimento <102737710+Lilianascimento@users.noreply.github.com>
2022-05-24 10:36:57 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,104 @@
+var paciente = document.querySelector('#primeiro-paciente')
+
+var ValorPeso = paciente.querySelector('.info-peso')
+var Peso = parseInt(ValorPeso.textContent)
+console.log (Peso)
+
+var ValorAltura = paciente.querySelector('.info-altura')
+var Altura = parseInt(ValorAltura.textContent)
+console.log (Altura)
+
+var ValorGordura = paciente.querySelector('.info-gordura')
+var Gordura = parseInt(ValorGordura.textContent)
+console.log (Gordura)
+
+var ValorIMC = (Peso + Altura + Gordura)/3;
+console.log (ValorIMC)
+
+var substituirIMC = paciente.querySelector('.info-imc')
+substituirIMC.textContent = ValorIMC.toFixed(0)
+console.log(ValorIMC)
+
+var paciente = document.querySelector('#segundo-paciente')
+
+var ValorPeso = paciente.querySelector('.info-peso')
+var Peso = parseInt(ValorPeso.textContent)
+console.log (Peso)
+
+var ValorAltura = paciente.querySelector('.info-altura')
+var Altura = parseInt(ValorAltura.textContent)
+console.log (Altura)
+
+var ValorGordura = paciente.querySelector('.info-gordura')
+var Gordura = parseInt(ValorGordura.textContent)
+console.log (Gordura)
+
+var ValorIMC = (Peso + Altura + Gordura)/3;
+console.log (ValorIMC)
+
+var substituirIMC = paciente.querySelector('.info-imc')
+substituirIMC.textContent = ValorIMC.toFixed(0)
+console.log(ValorIMC)
+
+var paciente = document.querySelector('#terceiro-paciente')
+
+var ValorPeso = paciente.querySelector('.info-peso')
+var Peso = parseInt(ValorPeso.textContent)
+console.log (Peso)
+
+var ValorAltura = paciente.querySelector('.info-altura')
+var Altura = parseInt(ValorAltura.textContent)
+console.log (Altura)
+
+var ValorGordura = paciente.querySelector('.info-gordura')
+var Gordura = parseInt(ValorGordura.textContent)
+console.log (Gordura)
+
+var ValorIMC = (Peso + Altura + Gordura)/3;
+console.log (ValorIMC)
+
+var substituirIMC = paciente.querySelector('.info-imc')
+substituirIMC.textContent = ValorIMC.toFixed(0)
+console.log(ValorIMC)
+
+var paciente = document.querySelector('#quarto-paciente')
+
+var ValorPeso = paciente.querySelector('.info-peso')
+var Peso = parseInt(ValorPeso.textContent)
+console.log (Peso)
+
+var ValorAltura = paciente.querySelector('.info-altura')
+var Altura = parseInt(ValorAltura.textContent)
+console.log (Altura)
+
+var ValorGordura = paciente.querySelector('.info-gordura')
+var Gordura = parseInt(ValorGordura.textContent)
+console.log (Gordura)
+
+var ValorIMC = (Peso + Altura + Gordura)/3;
+console.log (ValorIMC)
+
+var substituirIMC = paciente.querySelector('.info-imc')
+substituirIMC.textContent = ValorIMC.toFixed(0)
+console.log(ValorIMC)
+
+var paciente = document.querySelector('#quinto-paciente')
+
+var ValorPeso = paciente.querySelector('.info-peso')
+var Peso = parseInt(ValorPeso.textContent)
+console.log (Peso)
+
+var ValorAltura = paciente.querySelector('.info-altura')
+var Altura = parseInt(ValorAltura.textContent)
+console.log (Altura)
+
+var ValorGordura = paciente.querySelector('.info-gordura')
+var Gordura = parseInt(ValorGordura.textContent)
+console.log (Gordura)
+
+var ValorIMC = (Peso + Altura + Gordura)/3;
+console.log (ValorIMC)
+
+var substituirIMC = paciente.querySelector('.info-imc')
+substituirIMC.textContent = ValorIMC.toFixed(0)
+console.log(ValorIMC)
